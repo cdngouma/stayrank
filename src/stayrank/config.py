7c6f74b85de2284b218b85936da808e920ce397c
cdngouma <ngouma.chrys@gmail.com>
2026-07-03 00:52:52 -0400

    Added data sources

--- src/stayrank/config.py
+++ src/stayrank/config.py
@@ -7,4 +7,22 @@ RAW_DIR = DATA_DIR / "raw"
 PROCESSED_DIR = DATA_DIR / "processed"
 
 RAW_LISTINGS_PATH = RAW_DIR / "listings.csv.gz"
+RAW_STM_STOPS_PATH = RAW_DIR / "stm_stops.csv"
+RAW_DESTINATIONS_PATH = RAW_DIR / "montreal_destinations.txt"
+RAW_EVENTS_PATH = RAW_DIR / "montreal_events.csv"
+
 DB_PATH = DATA_DIR / "stayrank.duckdb"
+
+BOROUGHS_LIST = [
+    'Ville-Marie', 'Rosemont-La Petite-Patrie',
+    'Côte-des-Neiges-Notre-Dame-de-Grâce', 'Le Sud-Ouest',
+    'Le Plateau-Mont-Royal', 'Villeray-Saint-Michel-Parc-Extension',
+    "Baie-d'Urfé", 'Saint-Laurent', 'Mercier-Hochelaga-Maisonneuve',
+    'Lachine', 'Outremont', 'Ahuntsic-Cartierville', 'Westmount',
+    'Dorval', 'Anjou', 'Pointe-Claire', 'Verdun', 'Mont-Royal',
+    'LaSalle', 'Côte-Saint-Luc', "L'Île-Bizard-Sainte-Geneviève",
+    'Hampstead', 'Saint-Léonard', 'Beaconsfield',
+    'Dollard-des-Ormeaux', 'Pierrefonds-Roxboro', 'Montréal-Ouest',
+    'Rivière-des-Prairies-Pointe-aux-Trembles', 'Montréal-Nord',
+    'Kirkland', 'Montréal-Est', 'Sainte-Anne-de-Bellevue'
+]
